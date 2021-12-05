9921bbdd4ce15e0c4cf86e22d0633aaa6e0bc9c2
Tahmin Fatiyev <tahminshf@code.edu.az>
2021-12-06 03:06:46 +0400

    Calculating

---
+++ CalculatingNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Calculate();
+        }
+    }
+}
