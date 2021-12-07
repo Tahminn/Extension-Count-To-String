681f33b68ae1570c204364eb024800b203d8f289
Tahmin Fatiyev <tahminshf@code.edu.az>
2021-12-08 01:06:58 +0400

    Extension Count String

---
+++ HM.ExtensionToString/Helpers/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HM.ExtensionToString.Helpers
+{
+    public static class Extension
+    {
+        public static int PatternChecker(this string str, string pattern)
+        {
+            return Regex.Matches(str, pattern).Count;
+        }
+    }
+}
---
+++ HM.ExtensionToString/Program.cs
@@ -0,0 +1,16 @@
+﻿using HM.ExtensionToString.Helpers;
+using System;
+
+namespace HM.ExtensionToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Biz birlikde derse gede, birlikde oxuya ve yaza bilerik";
+            int checker = text.PatternChecker("birlikde");
+            Console.WriteLine(checker);
+
+        }
+    }
+}
